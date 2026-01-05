cad7588c3803ee06552163e34509df1fee6c3c8a
Raj Goyal <rajg50103@gmail.com>
2026-01-05 18:25:09 +0530

    Fix: Update catch-all route for Express 5 compatibility

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get('/api', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
